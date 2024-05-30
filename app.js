9f5ce15d0c236f71b1a21a4f8cc64538281da0e0
AStefankiv <stefankif35@gmail.com>
2024-05-29 22:02:31 -0600

    Add os

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const { john, peter } = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavor');
-require('./7-mind-grenade');
+const os = require('os');
 
-// sayHi('Susan');
-// sayHi(john);
-// sayHi(peter);
+// info about current user
+const user = os.userInfo();
+
+//method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem()
+}
+
+console.log(currentOS);
