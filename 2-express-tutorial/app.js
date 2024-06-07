3e4b2f5cdde45ea167e4db15e1e122981d8d1d26
AStefankiv <stefankif35@gmail.com>
2024-06-06 21:55:39 -0600

    Middleware function logger created

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  const status = res.statusCode;
+  console.log(`
+  Method: ${method},
+  URL: ${url},
+  Time: ${time},
+  Status: ${status}`
+);
+  next();
+};
+
+app.get('/', logger,(req, res) => {
+
   res.status(200).send('Home Page');
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', logger, (req, res) => {
   res.status(200).send('About Page');
 });
 
