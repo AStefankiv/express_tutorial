631c7ad4e07ec3d1f85630c4949b6cc5015af7f1
AStefankiv <stefankif35@gmail.com>
2024-06-07 20:50:23 -0600

    Install morgan npm

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,26 +1,7 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-const authorize = require('./authorize');
-
-app.use([logger, authorize]);
-
-app.get('/',(req, res) => {
-  res.status(200).send('Home Page');
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page');
-});
-
-app.get('/api/products', (req, res) => {
-  res.status(200).send('Products');
-});
 
 
-app.get('/api/items', (req, res) => {
-  res.status(200).send('Items');
-});
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....');
