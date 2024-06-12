36cceca3d7593fe5a9f53ff33847e600a13e6a6e
AStefankiv <stefankif35@gmail.com>
2024-06-11 21:13:51 -0600

    Add route functions into controllers/tasks.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
-app.get('/', (req, res) => {
+// middleware
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
   res.send('Task Manager App');
 });
+
+app.use('/api/v1/tasks', tasks);
+
+// app.get('/api/v1/tasks') => get all the tasks
+// app.post('/api/v1/tasks') => create a new task
+// app.get('/api/v1/tasks/:id') => get a single task
+// app.patch('/api/v1/tasks/:id') => update task
+// app.delete('/api/v1/tasks/:id') => delete task
 
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log('Server is running on port {port}');
+  console.log(`Server is listening on port ${port}...`);
 });
