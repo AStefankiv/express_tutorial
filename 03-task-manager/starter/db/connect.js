5e62568fc62f056c04350fdb7b57c2cbbd3ad01a
AStefankiv <stefankif35@gmail.com>
2024-06-13 22:19:05 -0600

    Create .env file

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-'mongodb+srv://stefankif35:1234@nodeexpressprojects.o3rdqge.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects'
 
-mongoose
-.connect(connectionString)
-.then(() => {console.log('Connected to the database');})
-.catch((err) => {console.log(err);});
+const connectDB = (url) => {
+  return mongoose.connect(url);
+}
+
+// mongoose
+// .connect(connectionString)
+// .then(() => {console.log('Connected to the database');})
+// .catch((err) => {console.log(err);});
+
+module.exports = connectDB;
