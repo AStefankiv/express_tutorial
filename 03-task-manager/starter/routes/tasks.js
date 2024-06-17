58a061b9f13c34699b47a935c11f5d082feff785
AStefankiv <stefankif35@gmail.com>
2024-06-16 19:27:30 -0600

    Create not-found middleware for non-existing routes

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -4,11 +4,6 @@ const router = express.Router();
 const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks');
 
 router.route('/').get(getAllTasks).post(createTask);
-//alternative:
-// router.get('/', getAllTasks);
-// router.post('/', createTask);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
-
-
 
 module.exports = router;
